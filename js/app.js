a5dd404b4a64e0e273ea03bd770b580ea7422d3c
Tim Hall <thall74@gatech.edu>
2017-08-10 21:02:08 -0700

    Add ajax functoin

--- js/app.js
+++ js/app.js
@@ -3,14 +3,35 @@ var ViewModel = function(username) {
     var self = this;
 
     self.username = ko.observable(username);
-    self.result = $.ajax({url: "demo_test.txt", success: function(result){
-                      $("#div1").html(result);
-                    }});
+
+    var t = testAjax('github', 'timjhall');
+    console.log("testAjax:");
+    console.log(t);
+
+
+    // self.result = testAjax('github', 'timjhall');
+
+    // self.result = ko.computed(function() {
+    //     return testAjax('github', self.username);
+    // }, self);
+
+
  
     // this.fullName = ko.pureComputed(function() {
     //     // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
     //     return this.firstName() + " " + this.lastName();
     // }, this);
 };
+
+
+
+function testAjax(site, username) {
+  if (site == 'github') {
+    return $.ajax({
+      url: `https://api.github.com/users/${username}`
+    });
+  }
+  
+}
  
 ko.applyBindings(new ViewModel("Username")); // This makes Knockout get to work
