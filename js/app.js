2ab80311e1c118bf844741b846e985fc584214a9
Tim Hall <thall74@gatech.edu>
2017-08-12 18:17:25 -0700

    Refactor app.js

--- js/app.js
+++ js/app.js
@@ -3,29 +3,11 @@ var ViewModel = function() {
     var self = this;
 
     self.username = ko.observable("");
-    // self.github = ko.observable("");
-
-    // var t = testAjax('github', 'timjhall');
-    // console.log("testAjax:");
-    // console.log(t);
-
-
-    // self.result = testAjax('github', 'timjhall');
-    // console.log(self.result);
-
+   
     self.github = ko.computed(function() {
         return testAjax('github', self.username());
     }, self);
-
-
- 
-    // this.fullName = ko.pureComputed(function() {
-    //     // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-    //     return this.firstName() + " " + this.lastName();
-    // }, this);
 };
-
-
 
 function testAjax(site, username) {
   if (username == "") {
